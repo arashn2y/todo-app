432c848d475e671e0d23c6b49a162b47a1d1855c
Arash N <arashn2y@gmail.com>
2024-10-17 16:21:06 +0200

    add login page

--- src/route.tsx
+++ src/route.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./screens/Dashboard";
+import Login from "./screens/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard />
+  },
+  {
+    path: "/login",
+    element: <Login />
   }
 ]);
 
---
+++ src/screens/Login.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/sections/Header";
+
+function Login() {
+  return (
+    <div className="grid grid-rows-12 h-screen">
+      <Header title="Login" />
+    </div>
+  );
+}
+
+export default Login;
